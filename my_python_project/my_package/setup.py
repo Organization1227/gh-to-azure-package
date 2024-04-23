9ca582949e883c3f88af3371fc8371ccaaa228c4
kanampalli <himaja.kanampalli@gmail.com>
2024-04-23 17:04:47 -0400

    Update setup.py

--- my_python_project/my_package/setup.py
+++ my_python_project/my_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="my_package",
-    version="0.3",
+    version="0.4",
     packages=find_packages(),
     install_requires=[],  # Add any dependencies here
 )
